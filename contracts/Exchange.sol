ecd9a6a90598bdeb487a25e8a7e2fb43b0c67c96
AmirAlSakban <100512905+AmirAlSakban@users.noreply.github.com>
2024-07-23 12:46:09 +0300

    13. fill orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
     mapping(uint256 => _Order) public orders; //order mapping
     uint256 public orderCount; //number of orders in the mapping 
     mapping(uint256 => bool) public orderCancelled; //mapping to check if an order has been cancelled
+    mapping(uint256 => bool) public orderFilled; //mapping to check if an order has been filled
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
@@ -31,6 +32,17 @@ contract Exchange {
         uint256 amountGet, 
         address tokenGive, 
         uint256 amountGive, 
+        uint256 timestamp
+    );
+
+    event Trade(
+        uint256 id, 
+        address user, 
+        address tokenGet, 
+        uint256 amountGet, 
+        address tokenGive, 
+        uint256 amountGive, 
+        address creator, 
         uint256 timestamp
     );
 
@@ -124,8 +136,8 @@ contract Exchange {
                 block.timestamp //time the order was created, using epoch time (seconds since 1970); block.timestamp is a global variable that gives the current block timestamp
             ); 
 
-        //require that the amount is greater than 0
-        require(_amountGet > 0 && _amountGive > 0);
+            //require that the amount is greater than 0
+            require(_amountGet > 0 && _amountGive > 0);
         }
 
         function cancelOrder(uint256 _id) public {
@@ -151,5 +163,70 @@ contract Exchange {
                 _order.amountGive,
                 block.timestamp
             );
+        }
+
+        //-------------------------
+        //EXECUTE ORDERS
+
+        function fillOrder(uint256 _id) public {
+            // 1. Must be valid orderId
+            require(_id > 0 && _id <= orderCount, "Order does not exist");
+            // 2. Order can't be already filled 
+            require(!orderFilled[_id]);
+            // 3. Order can't be cancelled
+            require(!orderCancelled[_id]);
+
+            //Fetch the order
+            _Order storage _order = orders[_id]; //storage is a keyword that tells the compiler to store the value in the state variable, not in memory
+
+            //execute the trade
+            _trade(
+                _order.id,
+                _order.user,
+                _order.tokenGet,
+                _order.amountGet,
+                _order.tokenGive,
+                _order.amountGive
+            );
+            
+            // Mark order as filled
+            orderFilled[_order.id] = true;
+        }
+
+        function _trade(
+            uint256 _orderId,
+            address _user,
+            address _tokenGet,
+            uint256 _amountGet,
+            address _tokenGive,
+            uint256 _amountGive
+        ) internal {
+            //fee paid by the user that fills the order, a fee is charged to the user that fills the order
+            //fee deducted from _amountGet
+            uint256 _feeAmount = (_amountGive * feePercent) / 100;
+
+            //trade logic here
+            //msg.sender is the user that fills the order, while _user is the user that created the order
+            tokens[_tokenGet][msg.sender] -= _amountGet + _feeAmount;
+            tokens[_tokenGet][_user] += _amountGet;
+
+            //charge fees
+            tokens[_tokenGet][feeAccount] += _feeAmount;
+
+            tokens[_tokenGive][_user] -= _amountGive;
+            tokens[_tokenGive][msg.sender] += _amountGive;
+
+            // Emit trade event
+            emit Trade(
+                _orderId,
+                msg.sender,
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                _user,
+                block.timestamp
+            );
+            
         }
 }
