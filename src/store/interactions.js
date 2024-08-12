59fb1f5168e1c6873d0312aaa23d3ed407b20a98
AmirAlSakban <100512905+AmirAlSakban@users.noreply.github.com>
2024-08-12 15:23:37 +0300

    22. Withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -55,7 +55,11 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Deposit', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS',event })
   })
-  }
+
+  exchange.on('Withdraw', (token, user, amount, balance, event) => {
+    dispatch({ type: 'TRANSFER_SUCCESS', event })
+  })
+}
       
 
 //---------------------------
@@ -83,9 +87,18 @@ export const transferTokens = async (provider, exchange, transferType, token, am
   let transaction
 
   dispatch({ type: 'TRANSFER_REQUEST' })
+
   try {
     const signer = await provider.getSigner()
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
+
+    if(transferType === 'Deposit') {
+      transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+      await transaction.wait()
+      transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    } else {
+      transaction = await exchange.connect(signer).withdrawToken(token.address, amountToTransfer)
+    }
   
     transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
     await transaction.wait()
