24e60fcc251cbf806bc10ab8d16a0c030ff568a4
AmirAlSakban <100512905+AmirAlSakban@users.noreply.github.com>
2024-09-01 15:17:11 +0300

    Polygon Amoy deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: { 
-    localhost: {}
+    localhost: {},
+    // kovan: {
+    //   url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    //   accounts: privateKeys.split(","),
+    // },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
+    amoy: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
   },
 };
